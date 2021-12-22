8a4cd8fef8f240df7a21336d3126f0b539ba4613
Mohit <mohitgautamnexus@gmail.com>
2021-12-22 20:11:04 +0530

    git push  protected

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Link,Routes } from "react-router-dom";
+import HomeComponent from "./pages/HomeComponent";
+import AccountComponent from "./pages/AccountComponent";
+import CardsComponent from "./pages/CardsComponent";
+// import ProtectedRoute from './ProtectedRoute';
+import useAuth from "./useAuth";
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="ui container">
+      <h2>Protected Routes Tutorial</h2>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/" >Home Page</Link>
+          </li>
+          <li>
+            <Link to="/accounts" >Accounts (protected)</Link>
+          </li>
+          <li>
+            <Link to="/cards" >Cards(unProtected)</Link>
+          </li>
+        </ul>
+      
+          <Routes>
+            <Route path="/" exact element={ <HomeComponent/>} ></Route>
+            <Route path="/accounts"  element={ <AccountComponent/>} ></Route>
+            <Route path="/cards"  element={< CardsComponent/>} ></Route>
+          </Routes>
+      </Router>
     </div>
   );
 }
